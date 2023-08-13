f83034bbbf3dde497960ae1acdaaf2ac30835c76
Manu Kumar Pal <manukumarpal34@gmail.com>
2023-08-13 11:22:41 +0530

    add sytling

--- todo-list/src/components/Home.js
+++ todo-list/src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 const Home = () => {
+    
   return (
     <>
+        <h1 className='h1'>To-do List</h1>
         <div className='main-container'>
-                <h2>Add Item here!</h2>
+                {/* <h2>Add Item here!</h2> */}
+                    {/* <img src='images/img.jpeg' alt='logo' className='log'/> */}
             <div> 
-                    <input type='text' placeholder='Add Item'></input>
+                    <input type='text' placeholder='Add Item..' className='inp'></input>
+                    <button type='button' className='btn'>Add</button>
             </div>
         </div>
     </>
